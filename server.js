33c2c7530f5302c1a644230d3014a224d7fabf6a
Mario C <touch.technologies.sv@gmail.com>
2024-04-21 21:12:30 -0600

    modified:   server.js

--- server.js
+++ server.js
@@ -1,48 +1,34 @@
-// Import required modules
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const url = require('url');
-
 const express = require('express');
-const { Pool } = require('pg');
-const cors = require('cors');
+const fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 8080;
+const app = express();
 
-const server = http.createServer((req, res) => {
-    // Parse the requested URL
-    const parsedUrl = url.parse(req.url, true);
-    let filePath = './public' + parsedUrl.pathname;
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
 
-    // If the requested path is '/', serve 'index.html' by default
-    if (filePath === './public') {
-        filePath = './public/login.html';
-    }
-
-    // Read the file and serve it
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            if (err.code === 'ENOENT') {
-                res.writeHead(404);
-                res.end('404 Not Found');
-            } else {
-                res.writeHead(500);
-                res.end('500 Internal Server Error');
-            }
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(data, 'utf-8');
-        }
+// If the requested path is '/', serve 'login.html' by default
+app.route('/')
+    .get((req, res) => {
+        res.sendFile(__dirname + '/public/login.html');
     });
+
+// Route for handling other paths
+app.route('*')
+    .get((req, res) => {
+        const filePath = __dirname + '/public' + req.path;
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                if (err.code === 'ENOENT') {
+                    res.status(404).send('404 Not Found');
+                } else {
+                    res.status(500).send('500 Internal Server Error');
+                }
+            } else {
+                res.status(200).send(data);
+            }
+        });
+    });
+
+app.listen(8080, () => {
+    console.log('Server is running on port 8080');
 });
-
-
-// http start server
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-
-
